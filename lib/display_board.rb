ed3adcf050b7dc0e9af09ca734c1331f8b919a76
Hendrick Salomon <hsalomon86@gmail.com>
2020-01-04 01:50:13 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board, num)
+  cellrow = " #{board[num]} | #{board[num]} | #{board[num]} "
+  lines = "-----------"
+puts cellrow
+puts lines
+puts cellrow
+puts lines
+puts cellrow
+end
